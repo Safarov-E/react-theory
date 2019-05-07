8f7e12b09df5bd706661f9912952352598fed50c
Elkhan <elkhan.safarov10@yandex.ru>
2019-05-07 22:26:06 +0500

    Создание простого компонента

---
+++ src/Car/Car.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+/* function Car() {
+	return (
+		<div> This is car component </div>
+	)
+} Аналогичный код*/ 
+
+/* const Car = () => {
+	return (
+		<div> This is car component </div>
+	)
+} Аналогичный код*/
+
+/* const Car = () =>  (
+	<div> 
+		This is car component 
+	</div>
+) Аналогичный код*/
+
+export default () => (
+	<div> 
+		This is car component 
+		<strong> Text </strong>
+	</div>
+)
