52f7da43a94d0afd6c877a0bbf2c7ee8dfdbf853
Elkhan <elkhan.safarov10@yandex.ru>
2019-05-07 23:50:47 +0500

    Передача контента

--- src/App.js
+++ src/App.js
@@ -10,8 +10,12 @@ class App extends Component {
     return (
       <div style={divStyle}>
         <h1>Hello world</h1>
-        <Car name={'Ford'} year={2018}/>
-        <Car name="Audi" year={2016}/>
+        <Car name={'Ford'} year={2018}>
+          <p style={{'color': 'red'}}> COLOR </p>
+        </Car>
+        <Car name="Audi" year={2016}>
+          <p style={{'color': 'blue'}}> COLOR </p>
+        </Car>
         <Car name={'Mazda'} year={2010}/>
       </div>
     );
