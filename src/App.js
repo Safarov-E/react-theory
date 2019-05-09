6278a90548c0712281db618d9c1bb1513e5e1dfb
Elkhan <elkhan.safarov10@yandex.ru>
2019-05-09 16:57:47 +0500

    Динамические списки

--- src/App.js
+++ src/App.js
@@ -20,8 +20,22 @@ class App extends Component {
     })
   }
 
-  changeTitleHandle = pageTitle => {
-    this.setState({pageTitle})
+  onChangeName(name, index) {
+    console.log(name, index)
+    const car = this.state.cars[index]
+    car.name = name
+    const cars =  [...this.state.cars]
+    cars[index] = car
+    this.setState({
+      cars
+    })
+  }
+
+  deleteHendler(index) {
+    const cars = this.state.cars.concat()
+    cars.splice(index, 1)
+
+    this.setState({cars})
   }
 
   render() {
@@ -38,7 +52,8 @@ class App extends Component {
                   key={index} 
                   name={car.name}
                   year={car.year}
-                  onChangeTitle={() => this.changeTitleHandle(car.name)}
+                  onDelete={this.deleteHendler}
+                  onChangeName={(event) => this.onChangeName(event.target.value, index)}
                 />
               )
             }) 
